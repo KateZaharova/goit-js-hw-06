1edecf6a38d7a076ac1d80ca9a6a700086838529
Kate Zaharova <15071981ktz@gmail.com>
2023-06-19 19:50:22 +0300

    task 03

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,21 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const imgList = document.querySelector(".gallery");
+
+const markup = images
+  .map((image) => `<li class="list-item">
+  <img src = "${image.url}", alt="${image.alt}" width=663px></img>
+  </li>`)
+  .join("");
+
+imgList.insertAdjacentHTML("beforeend", markup);
+
+
+
+/*
+Використовуй масив об'єктів images для створення елементів <img>, вкладених в <li>. Для створення розмітки використовуй шаблонні рядки і метод insertAdjacentHTML().
+
+Усі елементи галереї повинні додаватися в DOM за одну операцію додавання.
+Додай мінімальне оформлення галереї флексбоксами або грідами через CSS класи.*/
