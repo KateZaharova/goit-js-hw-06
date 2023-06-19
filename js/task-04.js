fa75d69f48ea7160d91f190dbcffb8f1d9fd2077
Kate Zaharova <15071981ktz@gmail.com>
2023-06-19 22:42:04 +0300

    task-04

---
+++ js/task-04.js
@@ -0,0 +1,26 @@
+const counter = document.querySelector("#value");
+let counterValue=0;
+
+const decrementButton = document.querySelector('button[data-action="decrement"]');
+const incrementButton = document.querySelector('button[data-action="increment"]')
+
+decrementButton.addEventListener("click", handlerMinusClick)
+incrementButton.addEventListener("click", handlerPlusClick)
+
+
+function handlerPlusClick() {
+    counterValue += 1
+    counter.textContent = `${counterValue}`;
+}
+
+
+function handlerMinusClick() {
+    counterValue -= 1
+    counter.textContent = `${counterValue}`;
+}
+
+
+/*
+Створи змінну counterValue, в якій буде зберігатися поточне значення лічильника та ініціалізуй її значенням 0.
+Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
+Оновлюй інтерфейс новим значенням змінної counterValue.*/
