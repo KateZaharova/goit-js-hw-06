eced30e53d7284828ef0279ec785e5aacf6f1e93
Kate Zaharova <15071981ktz@gmail.com>
2023-06-20 15:44:10 +0300

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,24 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const body = document.querySelector("body");
+const span = document.querySelector(".color");
+const button = document.querySelector(".change-color");
+
+
+function changeColor () {
+  body.style.backgroundColor = getRandomHexColor();
+  span.textContent = body.style.backgroundColor;
+}
+
+button.addEventListener("click", () => {
+  changeColor()
+})
+
+/*
+
+Напиши скрипт, який змінює кольори фону елемента <body> через інлайн-стиль по кліку на button.change-color і виводить значення кольору в span.color.
+
+*/
